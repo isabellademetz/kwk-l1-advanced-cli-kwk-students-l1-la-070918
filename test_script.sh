a14d6374c79ee0869acf5fddf25844a403272f3d
isabellademetz <isabella21136@gmail.com>
2018-07-09 21:06:57 +0000

    automatically backed up by learn

---
+++ test_script.sh
@@ -0,0 +1,6 @@
+ls;
+mkdir this_is_a_directory_ma de_in_a_script;
+mkdir another_directory;
+touch this_is_a_directory_ma de_in_a_script/first_f ile.txt;
+tough this_is_a_directory_ma de_in_a_script/second_ file.txt;
+mv this_is_a_directory_ma de_in_a_script/second_ file.txt another_directory
